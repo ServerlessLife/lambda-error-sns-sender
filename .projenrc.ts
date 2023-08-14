b52c88a0bb00db8029b2c0caed71212e094f2650
Marko (ServerlessLife) <marko@serverlesslife.com>
2023-08-14 20:58:52 +0200

    Initial commit

---
+++ .projenrc.ts
@@ -0,0 +1,135 @@
+import { awscdk } from 'projen';
+import { TaskWorkflow } from 'projen/lib/github';
+import { NodePackageManager } from 'projen/lib/javascript';
+
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Marko (ServerlessLife)',
+  authorAddress: 'marko@serverlesslife.com',
+  cdkVersion: '2.70.0',
+  defaultReleaseBranch: 'main',
+  jsiiVersion: '^5.1.9',
+  name: 'lambda-error-sns-sender',
+  projenrcTs: true,
+  repositoryUrl:
+    'https://github.com/ServerlessLife/lambda-error-sns-sender.git',
+  eslint: true,
+  packageManager: NodePackageManager.NPM,
+  prettier: true,
+  prettierOptions: {
+    settings: {
+      useTabs: false,
+      singleQuote: true,
+    },
+  },
+  deps: [],
+  description: 'CDK construct to send Lambda detail errors to AWS SNS Topic.',
+  devDeps: ['typescript', 'esbuild', 'esbuild-runner'],
+  jest: false,
+  gitignore: ['/cdk.out/'],
+  keywords: [
+    'aws',
+    'cdk',
+    'lambda',
+    'sns',
+    'error',
+    'cloudwatch',
+    'logs',
+    'serverless',
+    'alarm',
+    'monitoring',
+    'email',
+  ],
+  publishToPypi: {
+    distName: 'lambda-error-sns-sender',
+    module: 'lambda-error-sns-sender',
+  },
+  publishToNuget: {
+    packageId: 'ServerlessLife.LambdaErrorSnsSender',
+    dotNetNamespace: 'ServerlessLife.LambdaErrorSnsSender',
+  },
+  publishToMaven: {
+    mavenGroupId: 'io.github.serverlesslife',
+    javaPackage: 'io.github.serverlesslife.LambdaErrorSnsSender',
+    mavenArtifactId: 'LambdaErrorSnsSender',
+    mavenEndpoint: 'https://s01.oss.sonatype.org',
+  },
+  publishToGo: {
+    moduleName: 'github.com/ServerlessLife/lambda-error-sns-sender',
+  },
+});
+
+project.tsconfigDev.include.push('functions/**/*.ts', 'scripts/**/*.ts');
+project.package.addField('workspaces', ['functions', 'scripts']);
+project.gitignore.exclude('!functions/**/tsconfig.json');
+project.gitignore.exclude('!scripts/**/tsconfig.json');
+
+const esbuilTask = project.addTask('esbuild', {
+  exec: `
+  esbuild --bundle --minify --platform=node --external:@aws-sdk --format=esm functions/lambdaSnsError/index.ts --outfile=lib/functions/lambdaSnsError/index.mjs
+  cp functions/package.json lib/functions/lambdaSnsError
+  `,
+});
+
+project.tasks.tryFind('post-compile')!.prependSpawn(esbuilTask);
+
+// export raw CloudFormation for independent use (see part 3 of the article)
+project.addTask('export-cf', {
+  steps: [
+    {},
+    {
+      exec: `
+        npx cdk synth lambda-error-sns-sender-cf --quiet
+        npx esr scripts/copyToS3.ts
+  `,
+    },
+  ],
+});
+
+(
+  project.github?.workflows.find((x) => x.name === 'release') as TaskWorkflow
+).addJob('release_raw_cloudformation', {
+  name: 'Publish raw CloudFormation',
+  needs: ['release'],
+  runsOn: ['ubuntu-latest'],
+  permissions: {},
+  steps: [
+    {
+      name: 'Checkout',
+      uses: 'actions/checkout@v3',
+      with: {
+        'fetch-depth': 0,
+      },
+    },
+    {
+      name: 'Setup Node.js',
+      uses: 'actions/setup-node@v3',
+      with: {
+        'node-version': '16.x',
+      },
+    },
+    {
+      name: 'Install dependencies',
+      run: 'npm ci',
+    },
+    {
+      name: 'Build',
+      run: 'npx projen build',
+    },
+    {
+      name: 'Configure AWS credentials',
+      uses: 'aws-actions/configure-aws-credentials@v2',
+      with: {
+        'aws-access-key-id': '${{ secrets.AWS_ACCESS_KEY_ID }}',
+        'aws-secret-access-key': '${{ secrets.AWS_SECRET_ACCESS_KEY }}',
+        'aws-region': 'eu-west-1',
+      },
+    },
+    {
+      name: 'Export CF',
+      run: 'npx projen export-cf',
+    },
+  ],
+}),
+  console.log();
+
+project.synth();
---
+++ functions/lambdaSnsError/index.ts
@@ -0,0 +1,146 @@
+import { Buffer } from 'node:buffer';
+import {
+  CloudWatchLogsClient,
+  FilterLogEventsCommand,
+} from '@aws-sdk/client-cloudwatch-logs';
+import {
+  SNSClient,
+  PublishCommand,
+  PublishCommandInput,
+} from '@aws-sdk/client-sns';
+import { SNSEvent, Context } from 'aws-lambda';
+
+const cloudWatchLogsClient = new CloudWatchLogsClient({});
+const snsClient = new SNSClient({});
+const maxNumberOfLogs = process.env.MAX_NUMBER_OF_LOGS
+  ? parseInt(process.env.MAX_NUMBER_OF_LOGS)
+  : 100;
+
+export const handler = async (event: SNSEvent, context: Context) => {
+  try {
+    const records = event.Records.filter(
+      (r) => r.Sns.Subject?.startsWith('ALARM:'),
+    ).map((r) => ({
+      message: JSON.parse(r.Sns.Message),
+      topicArn: r.Sns.TopicArn,
+      timestamp: new Date(r.Sns.Timestamp),
+    }));
+
+    let metrics: {
+      functionName: string;
+      periodTotal: number;
+      topicArn: string;
+      timestamp: Date;
+    }[] = records
+      .filter(
+        (m) =>
+          m.message.Trigger &&
+          m.message.Trigger.Period &&
+          m.message.Trigger.Dimensions?.length === 1 &&
+          m.message.Trigger.Dimensions[0]?.name === 'FunctionName' &&
+          m.message.Trigger.Dimensions[0]?.value &&
+          m.topicArn,
+      )
+      .map((m) => ({
+        functionName: m.message.Trigger.Dimensions[0].value as string,
+        topicArn: m.topicArn as string,
+        periodTotal: ((m.message.Trigger.Period ?? 60) *
+          (m.message.Trigger.EvaluationPeriods ?? 1)) as number,
+        timestamp: m.timestamp,
+      }));
+
+    // remove duplicates
+    const functionNames = [...new Set(metrics.map((m) => m.functionName))];
+
+    if (functionNames.length === 0) {
+      return;
+    }
+
+    for (const functionName of functionNames) {
+      if (functionName === context.functionName) {
+        continue; //prevent recursion
+      }
+
+      const periodTotal = metrics.find(
+        (m) => m.functionName === functionName,
+      )!.periodTotal;
+
+      const timestamp = metrics.find(
+        (m) => m.functionName === functionName,
+      )!.timestamp;
+
+      const logsFromDate = new Date(
+        // 90 second safety margin
+        timestamp.getTime() - (periodTotal + 90) * 1000,
+      );
+
+      const logGroupName = await getLogGroupName(functionName);
+
+      //read CloudWatch logs
+      const logs: string[] = [];
+
+      let nextToken: string | undefined;
+
+      do {
+        const filterLogCommand = new FilterLogEventsCommand({
+          logGroupName: logGroupName,
+          filterPattern: 'ERROR',
+          startTime: logsFromDate.getTime(),
+          nextToken,
+          limit: maxNumberOfLogs - logs.length,
+        });
+
+        const cloudWatchCLogs = await cloudWatchLogsClient.send(
+          filterLogCommand,
+        );
+        logs.push(
+          ...(cloudWatchCLogs.events?.map((e) => `${e.message}`) ?? []),
+        );
+
+        if (logs.length >= maxNumberOfLogs) {
+          break;
+        }
+
+        nextToken = cloudWatchCLogs.nextToken;
+      } while (nextToken);
+
+      //const joinedLogs = `LAMBDA ${functionName} ERRORS:\n\n${logs.join('\n')}`;
+      const joinedLogs = logs.join('\n');
+
+      let stringBuffer = Buffer.from(joinedLogs, 'utf-8');
+      const maxLength = 240000; // actual max is 262144;
+      stringBuffer = Buffer.from(
+        stringBuffer.buffer,
+        stringBuffer.byteOffset,
+        Math.min(stringBuffer.length, maxLength),
+      );
+
+      //get topicArn from metrics
+      const topicArn = metrics.find((m) => m.functionName === functionName)
+        ?.topicArn;
+
+      if (!topicArn) {
+        throw new Error('TopicArn not found');
+      }
+
+      const input: PublishCommandInput = {
+        TopicArn: topicArn,
+        Subject: `Lambda ${functionName} errors`,
+        Message: stringBuffer.toString('utf-8'),
+      };
+
+      const command = new PublishCommand(input);
+
+      await snsClient.send(command);
+    }
+  } catch (error) {
+    // this Lambda should not throw any error
+    console.error(error);
+  }
+};
+
+async function getLogGroupName(
+  lambdaName: string,
+): Promise<string | undefined> {
+  return `/aws/lambda/${lambdaName}`;
+}
